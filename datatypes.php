59d7a556c83f9cb19acd2f7b2ea6772fd2fe2698
tahirkhan0332 <tahirkhan.dream@gmail.com>
2017-04-15 20:14:18 +0500

    Added file about datatypes in php

---
+++ datatypes.php
@@ -0,0 +1,27 @@
+<?php
+
+// string
+
+$name = "Tahir khan";
+echo $name;
+
+// integer
+
+$num = "8986797";
+echo $num;
+
+// boolian
+
+$value1 = "10";
+$value2 = "5";
+
+if ($value1 > $value2)
+{
+	echo "this statement is true";
+}
+else
+{
+	echo "not true";
+}
+
+?>
