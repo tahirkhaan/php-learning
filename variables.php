479ea9026777dac6a379ad7b70746efa2b59d6de
tahirkhan0332 <tahirkhan.dream@gmail.com>
2017-04-15 18:58:31 +0500

    Added files about variables in php

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Tahir khan";
+$email = "tahirkhan.dream@gmail.com";
+$roll_num = "26833";
+$age = "24";
+$result = "$name<br /> $email<br /> $roll_num<br /> $age";
+echo "$result";
+
+// OR
+// "<b>your name is:<b>" .$name. "<br /><br /> <b> this is my email:<b>"  .$email. "<br /> <br /><b> this is my roll number:<b>" .$roll_num. "<br /> <br /> this is my age:<b>" .$age;
+
+?>
+
