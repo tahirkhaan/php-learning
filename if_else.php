0c4324f91fc82b0daa91886b1acf5594e038470d
tahirkhan0332 <tahirkhan.dream@gmail.com>
2017-04-15 18:56:37 +0500

    Added file about conditional statement

---
+++ if_else.php
@@ -0,0 +1,25 @@
+<?php
+$city = "Mansehra";
+$number = "03328986797";
+
+if ($city == "abbottabad")
+{
+	echo "<script>alert('Abbottabad is my city')</script>";
+}
+else
+{
+	echo "<script>alert('abbottabad is not found')</script>";
+}
+
+// number
+
+if ($number == "03429855439")
+{
+	echo "<script>alert('THIS NUMBER IS OF TAHIR KHAN')</script>";
+}
+else
+{
+	echo "<script>alert('This number is not mine')</script>";
+}
+
+?>
