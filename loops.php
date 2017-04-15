177b131a21fa05a9d852b75d804eeb7231198ce3
tahirkhan0332 <tahirkhan.dream@gmail.com>
2017-04-15 23:00:18 +0500

    Added basic file about loops in php

---
+++ loops.php
@@ -0,0 +1,23 @@
+<?php
+
+// Loops are following: while ,for, foreach, and do while
+
+$num = 0;
+
+while ($num <= 100)
+{
+	echo "<br />" . $num++;
+}
+
+?>
+
+<?php
+
+// for loop example
+
+<?php  
+for ($x = 0; $x <= 10; $x++) {
+	echo "The number is: $x <br>";
+}
+
+?>
