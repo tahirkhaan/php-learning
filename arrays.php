cfca38604ecd5512b90e79c671731c8fcdf42527
tahirkhan0332 <tahirkhan.dream@gmail.com>
2017-04-15 21:24:27 +0500

    Added arrays file in php

---
+++ arrays.php
@@ -0,0 +1,12 @@
+<?php
+$info = array(
+	"Tahir khan",
+	"25",
+	"abbottbad",
+	"Computer Science",
+	"tahirkhan.dream@gmial.com",
+	"male"
+	);
+echo "<b>Name of the student:</b><br />" . $info[0] . "<br /><b>Age of the student:</b><br />" . $info[1] . "<br /><b>city of of the student:</b><br />" . $info[2] . "<br /><b>subject  of the student:</b><br />" . $info[3] . "<br /><b>email of the student:</b><br />" . $info[4] . "<br /><b>gender of the student:</b><br />" . $info[5] . "<br />";
+echo count($info);
+?>
